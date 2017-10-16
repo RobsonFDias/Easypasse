465698de980041c0371236accba93b0055359b61
Gustavo Severo <Gustavo@MacBook-Pro.local>
2017-10-16 21:27:48 -0200

    Criada tela de cadastro e botão de volta

--- app/src/main/java/com/example/gustavo/easypasse/LoginActivity.java
+++ app/src/main/java/com/example/gustavo/easypasse/LoginActivity.java
@@ -1,11 +1,14 @@
 package com.example.gustavo.easypasse;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -16,7 +19,19 @@ public class LoginActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Button acessar = (Button) findViewById(R.id.btnAcessar);
         
     }
 
+
+    public void cadastro(View view) {
+        Intent cadastro = new Intent(LoginActivity.this, CadastroActivity.class);
+        startActivity(cadastro);
+    }
+
+    public void acessar(View view) {
+        EditText cpf = (EditText) findViewById(R.id.edtCpf);
+        EditText senha = (EditText) findViewById(R.id.edtSenha);
+        Toast.makeText(this, "CPF "+cpf.getText().toString()+" senha "+senha.getText().toString(), Toast.LENGTH_LONG).show();
+    }
 }
