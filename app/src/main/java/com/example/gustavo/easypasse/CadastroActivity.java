8552b0b8591daca788686a51a2eb0127378cf5c5
Gustavo Severo <gustavo.msevero@gmail.com>
2017-12-07 20:53:01 -0200

    Criado Utilitarios.java

--- app/src/main/java/com/example/gustavo/easypasse/CadastroActivity.java
+++ app/src/main/java/com/example/gustavo/easypasse/CadastroActivity.java
@@ -6,9 +6,22 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class CadastroActivity extends AppCompatActivity {
+
+    private Button botaoSalvar;
+    private EditText usuario;
+    private EditText cpf;
+    private EditText senha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +30,40 @@ public class CadastroActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        botaoSalvar = (Button) findViewById(R.id.btnSalvar);
 
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                usuario = (EditText) findViewById(R.id.edtUsuario);
+                cpf = (EditText) findViewById(R.id.edtCpf);
+                senha = (EditText) findViewById(R.id.edtSenha);
+                Log.d("dados", "usuario "+usuario.getText().toString()+
+                        " cpf "+cpf.getText().toString()+
+                        " senha "+Utilitarios.md5(senha.getText().toString()));
+
+                JSONObject dadosJsonObject = new JSONObject();
+                try {
+                    dadosJsonObject.put("username", usuario.getText().toString() );
+                    dadosJsonObject.put("cpf", cpf.getText().toString() );
+                    dadosJsonObject.put("senha", Utilitarios.md5(senha.getText().toString()) );
+                    //dadosJsonObject.put("method", "app-get-login" );
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        });
     }
+
+
 
     public void voltarLogin(View view) {
         Intent voltar = new Intent(CadastroActivity.this, LoginActivity.class);
         startActivity(voltar);
     }
+
+
 }
--- app/src/main/java/com/example/gustavo/easypasse/LoginActivity.java
+++ app/src/main/java/com/example/gustavo/easypasse/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.gustavo.easypasse;
 
+import android.app.DownloadManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -29,12 +38,33 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(cadastro);
     }
 
-    public void acessar(View view) {
-        EditText cpf = (EditText) findViewById(R.id.edtCpf);
+    public void acessar(View view) throws JSONException {
+        EditText username = (EditText) findViewById(R.id.edtCpf);
         EditText senha = (EditText) findViewById(R.id.edtSenha);
-        Toast.makeText(this, "CPF "+cpf.getText().toString()+" senha "+senha.getText().toString(), Toast.LENGTH_LONG).show();
+//        Toast.makeText(this, "CPF "+username.getText().toString()+
+//                " senha "+senha.getText().toString(), Toast.LENGTH_LONG).show();
 
-        Intent intentPrincipal = new Intent(LoginActivity.this, PrincipalActivity.class);
-        startActivity(intentPrincipal);
+        JSONObject dadosJsonObject = new JSONObject();
+        dadosJsonObject.put("username", username.getText().toString());
+        dadosJsonObject.put("senha", Utilitarios.md5(senha.getText().toString()));
+        dadosJsonObject.put("method", "app-get-login" );
+        dadosJsonObject.toString();
+        Log.d("Dados enviados", dadosJsonObject.toString());
+
+        final JsonObjectRequest json = new JsonObjectRequest(Request.Method.POST,
+                "http://easypasse.com.br/gestao/wsLogin.php", dadosJsonObject, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("Response", response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Erro", error.toString());
+            }
+        });
+
+//        Intent intentPrincipal = new Intent(LoginActivity.this, PrincipalActivity.class);
+//        startActivity(intentPrincipal);
     }
 }
