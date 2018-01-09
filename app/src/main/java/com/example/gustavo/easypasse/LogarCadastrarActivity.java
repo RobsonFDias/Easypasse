169d05791f07c5c7986fe30de492cc5c850da960
Gustavo Severo <gustavo.msevero@gmail.com>
2018-01-09 17:02:39 -0200

    Ajuste no botão Acessar. Adicionado código de login na tela nova.

--- app/src/main/java/com/example/gustavo/easypasse/LogarCadastrarActivity.java
+++ app/src/main/java/com/example/gustavo/easypasse/LogarCadastrarActivity.java
@@ -7,12 +7,27 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LogarCadastrarActivity extends AppCompatActivity {
 
     private Button botaoacessar;
     private Button botaoCadastrar;
+
+    private EditText cpf;
+    private EditText senha;
+    private RequestQueue mVolleyRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +36,59 @@ public class LogarCadastrarActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        cpf = (EditText) findViewById(R.id.edtCpf);
+        senha = (EditText) findViewById(R.id.edtSenha);
+
+        mVolleyRequest = Volley.newRequestQueue(this);
+
         botaoacessar = (Button) findViewById(R.id.btnAcessar);
         botaoCadastrar = (Button) findViewById(R.id.btnCadastre_se);
 
         botaoacessar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                cpf = (EditText) findViewById(R.id.edtCpf);
+                senha = (EditText) findViewById(R.id.edtSenha);
+
+                JSONObject dadosJson = new JSONObject();
+                try {
+                    dadosJson.put("cpf", cpf.getText().toString());
+                    dadosJson.put("cpf", cpf.getText().toString());
+                    dadosJson.put("senha", Utilitarios.md5(senha.getText().toString()));
+                    dadosJson.put("method", "app-get-login");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                dadosJson.toString();
+                //Log.d("Dados que vão", dadosJson.toString());
+
+                final JsonObjectRequest json = new JsonObjectRequest(Request.Method.POST,
+                        "http://easypasse.com.br/gestao/wsLogin.php", dadosJson,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Log.d("Retorno", response.toString());
+
+                                try {
+                                    String msgAlerta = response.getString("msg");
+                                    Toast.makeText(LogarCadastrarActivity.this, msgAlerta, Toast.LENGTH_LONG).show();
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.d("TAG", "ERRO!!: " + error);
+                            }
+                        });
+
+                mVolleyRequest.add(json);
+
                 Intent principalIntent = new Intent(LogarCadastrarActivity.this, PrincipalActivity.class);
                 startActivity(principalIntent);
             }
