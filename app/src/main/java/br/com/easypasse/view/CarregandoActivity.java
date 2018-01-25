f919075553cb3c3418b107ebac5e7dd3f86f5fe4
Gustavo Severo <gustavo.msevero@gmail.com>
2018-01-25 19:15:24 -0200

    Ajustando o método do progressBar

--- app/src/main/java/br/com/easypasse/view/CarregandoActivity.java
+++ app/src/main/java/br/com/easypasse/view/CarregandoActivity.java
@@ -1,47 +1,18 @@
 package br.com.easypasse.view;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import android.widget.ProgressBar;
 
 import br.com.easypasse.R;
-import br.com.easypasse.config.EndPoints;
-import br.com.easypasse.controller.UsuarioControle;
-import br.com.easypasse.dao.DatabaseHelper;
-import br.com.easypasse.dao.DatabaseManager;
-import br.com.easypasse.model.UsuarioModelo;
-import br.com.easypasse.utils.ObjetosTransitantes;
-import br.com.easypasse.utils.Utilitarios;
 
 public class CarregandoActivity extends AppCompatActivity {
-
-    private Button botaoacessar;
-    private Button botaoCadastrar;
-
-    private EditText cpf;
-    private EditText senha;
-    private RequestQueue mVolleyRequest;
-    private String msgAlerta, retorno;
-    private JSONArray jsonArray;
-    private ProgressDialog pDialog;
+    private ProgressBar progressBar;
+    private Integer contador = 0;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +21,49 @@ public class CarregandoActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
 
+iniciaProgress();
+    }
+
+    private void iniciaProgress(){
+        try{
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (contador < 100){
+                        contador+=1;
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                progressBar.setProgress(contador);
+                            }
+                        });
+                        if(contador == 99){
+                            abrirTelaLogin();
+                        }
+
+                        try {
+                            Thread.sleep(200);
+                        }catch (InterruptedException i){
+                            i.printStackTrace();
+                        }
+
+                    }
+                }
+            }).start();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void abrirTelaLogin(){
+        try {
+            startActivity(new Intent(CarregandoActivity.this,LogarCadastrarActivity.class));
+            finish();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 }
